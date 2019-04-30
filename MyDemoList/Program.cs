6a2632a785255ef4009c3cc9691ec6af60bec46b
StepanHokobyan <31405336+StepanHokobyan@users.noreply.github.com>
2019-04-30 16:06:39 +0400

    Add project files.

---
+++ MyDemoList/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyDemoList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
